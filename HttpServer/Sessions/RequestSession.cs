dd27464d3b5b6c2e61ac583c760e9623ac1bc63c
Uwe Riegel <uriegel@hotmail.de>
2018-08-03 12:37:13 +0200

    RequestSession started

--- HttpServer/Sessions/RequestSession.cs
+++ HttpServer/Sessions/RequestSession.cs
@@ -1,20 +1,96 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using HttpServer.Exceptions;
+using HttpServer.Headers;
 using HttpServer.Interfaces;
 using HttpServer.WebSockets;
 
 namespace HttpServer.Sessions
 {
-    class RequestSession : ISession
+    class RequestSession : ISession, IDisposable
     {
+        #region Properties
+
         public string Id { get; }
-
         public static Counter Instances { get; } = new Counter();
+        public Server Server { get; }
+        public SocketSession SocketSession { get; }
+        public IPEndPoint LocalEndPoint { get; private set; }
+        public IPEndPoint RemoteEndPoint { get; private set; }
 
-        public Server Server { get; private set; }
+        #endregion
+
+        #region Constructor
+
+        public RequestSession(Server server, SocketSession socketSession, Stream networkStream)
+        {
+            Id = socketSession.Id + "-" + Interlocked.Increment(ref idSeed);
+            responseHeaders = new ServerResponseHeaders(server, Id);
+            Instances.Increment();
+            stopwatch = new Stopwatch();
+
+            this.SocketSession = socketSession;
+            this.Server = server;
+            this.networkStream = networkStream;
+
+            this.LocalEndPoint = SocketSession?.Client?.Client?.LocalEndPoint as IPEndPoint;
+            this.RemoteEndPoint = SocketSession?.Client?.Client?.RemoteEndPoint as IPEndPoint;
+
+            Logger.Current.LowTrace(() => $"{Id} New request session created - {RemoteEndPoint}");
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns>True: In dieser SocketSession weitermachen, ansonsten Verarbeitung abbrechen</returns>
+        public async Task<bool> StartAsync()
+        {
+            try
+            {
+                var read = await networkStream.ReadAsync(readBuffer, 0, readBuffer.Length);
+                if (read == 0)
+                {
+                    Logger.Current.LowTrace(() => $"{Id} Socket session closed");
+                    return false;
+                }
+                return await ReceiveAsync(read);
+            }
+            catch (Exception e) when (e is IOException || e is ConnectionClosedException || e is SocketException)
+            {
+                Logger.Current.LowTrace(() => $"{Id} Closing socket session: {e}");
+                Close(true);
+                return false;
+            }
+            catch (Exception e) when (e is ObjectDisposedException)
+            {
+                Logger.Current.Trace($"{Id} Object disposed");
+                Close(true);
+                return false;
+            }
+            catch (Exception e)
+            {
+                Logger.Current.Warning($"{Id} An error has occurred while reading socket: {e}");
+                Close(true);
+                return false;
+            }
+            finally
+            {
+                Instances.DecrementActive();
+            }
+        }
+
 
         public Service GetServiceInfo()
             => throw new NotImplementedException();
@@ -52,5 +128,223 @@ namespace HttpServer.Sessions
             //await WriteAsync(bytes, 0, bytes.Length);
         }
 
+        async Task<bool> ReceiveAsync(int bufferPosition)
+        {
+            try
+            {
+                //Headers = new RequestHeaders();
+                //var result = await (Headers as Headers).InitializeAsync(networkStream, readBuffer, bufferPosition, Server.Configuration.HeaderTracing, Id);
+                //bufferEndPosition = result.BufferEndPosition;
+                //readFromBuffer = bufferEndPosition > 0;
+                //bufferReadCount = result.BufferReadCount;
+
+                //Logger.Current.Trace($"{Id} Request: {RemoteEndPoint} \"{Headers.Method} {Headers.Url} {Headers.Http}\"");
+
+                //stopwatch?.Start();
+
+                ////string refererPath = null;
+                ////if (headers.ContainsKey("Referer"))
+                ////{
+                ////    string path = headers["Referer"];
+                ////    path = path.Substring(path.IndexOf("//") + 2);
+                ////    refererPath = path.Substring(path.IndexOf('/') + 1);
+                ////}
+
+                //if (Headers.Method == Web.Method.OPTIONS)
+                //    return ServeOptions();
+
+                //string query = null;
+                //var asyncExtensionResult = CheckAsyncExtension(Headers.Url);
+                //if (asyncExtensionResult.HasValue)
+                //{
+                //    var path = asyncExtensionResult.Value.Url;
+                //    query = Headers.Url.Substring(path.Length).Trim('/');
+                //    return await asyncExtensionResult.Value.Extension.RequestAsync(this, Headers.Method, path, query) && !isClosed;
+                //}
+
+                //var extensionResult = CheckExtension(Headers.Url);
+                //if (extensionResult != null)
+                //    return await ThreadTask<bool>.RunAsync(() =>
+                //    {
+                //        var path = extensionResult.Value.Url;
+                //        query = Headers.Url.Substring(path.Length).Trim('/');
+                //        return extensionResult.Value.Extension.Request(this, Headers.Method, path, query) && !isClosed;
+                //    });
+
+                //var isapi = CheckIsapi(Headers.Url);
+                //if (isapi != null)
+                //    if (await ThreadTask<bool>.RunAsync(() =>
+                //    {
+                //        var request = isapi.CreateRequest();
+                //        string path;
+                //        var pos = Headers.Url.IndexOf('?');
+                //        if (pos != -1)
+                //        {
+                //            query = Headers.Url.Substring(pos + 1);
+                //            path = Headers.Url.Substring(0, pos);
+                //        }
+                //        else
+                //            path = Headers.Url;
+
+                //        if (isapi.Url.Length == path.Length)
+                //            path = null;
+                //        else if (isapi.Url == "/" && (path?.StartsWith("/") ?? false))
+                //            path = path.Substring(1);
+
+                //        else if (isapi.Url.Length > 1 && isapi.Url.Length < path.Length)
+                //            path = path.Substring(isapi.Url.Length + 1);
+
+                //        if (!string.IsNullOrEmpty(path))
+                //            path = Uri.UnescapeDataString(path);
+
+                //        lock (locker)
+                //        {
+                //            if (request.Request(this, Headers.Method, Headers.ContentType, TranslatedPathOverride, path, query))
+                //            {
+                //                request.SetInfo(responseHeaders);
+                //                return true;
+                //            }
+                //        }
+                //        return false;
+                //    }))
+                //        return true;
+
+                //var proxyRedirection = Server.Configuration.CheckProxyRedirection(Headers.Url);
+                //if (proxyRedirection != null)
+                //    return await RedirectAsync(proxyRedirection.ExtractOriginalUrl(Headers.Url), true);
+
+                //var redirection = CheckRedirection(Headers.Url);
+                //if (redirection != null)
+                //{
+                //    if (redirection.IsProxy())
+                //        return await RedirectAsync(redirection.GetRedirectedUrl(Headers.Url));
+                //    else
+                //    {
+                //        await RedirectDirectoryAsync(redirection.GetRedirectedUrl(Headers.Url));
+                //        return true;
+                //    }
+                //}
+
+                //if (Headers.Method == Web.Method.POST && Headers.ContentType == "application/x-www-form-urlencoded" ||
+                //    Headers.Method == Web.Method.GET && Headers.Url.Contains('?'))
+                //{
+                //    var formHandler = CheckForm(Headers.Url);
+                //    if (formHandler != null)
+                //    {
+                //        var queryComponents = new Web.UrlQueryComponents(Headers.Method == Web.Method.POST ? $"{Headers.Url}?{ReadString()}" : Headers.Url);
+                //        await SendHtmlStringAsync(await formHandler.OnSubmitAsync(this, Headers.Method, Headers.Url, queryComponents));
+                //        return true;
+                //    }
+                //}
+
+                //if (SendInternalURL(Headers.Url))
+                //    return true;
+
+                //if (!CheckFile(Headers.Url, out string redirURL, out string localFile, out string pathInfo, out string queryString))
+                //{
+                //    await SendNotFoundAsync();
+                //    return true;
+                //}
+
+                //if (!String.IsNullOrEmpty(redirURL))
+                //{
+                //    await RedirectDirectoryAsync(redirURL);
+                //    return true;
+                //}
+
+                //var isapiScript = Server.Configuration.IsapiScripts?.FirstOrDefault(n => (localFile.EndsWith(n.Extension, StringComparison.OrdinalIgnoreCase)));
+                //if (isapiScript != null && await ThreadTask<bool>.RunAsync(() => SendIsapiScript(isapiScript, localFile, pathInfo, queryString)))
+                //    return true;
+
+                //var fastCGIScript = Server.Configuration.FastCGIScripts?.FirstOrDefault(n => (localFile.EndsWith(n.Extension, StringComparison.OrdinalIgnoreCase)));
+                //if (fastCGIScript != null && await ThreadTask<bool>.RunAsync(() => SendFastCGIScript(fastCGIScript, localFile, pathInfo, queryString)))
+                //    return true;
+
+                //await SendFileAsync(localFile);
+                return true;
+            }
+            catch (SocketException se)
+            {
+                if (se.SocketErrorCode == SocketError.TimedOut)
+                {
+                    Logger.Current.Warning($"{Id} Socket session closed, Timeout has occurred");
+                    Close(true);
+                    return false;
+                }
+                return true;
+            }
+            catch (ConnectionClosedException)
+            {
+                Logger.Current.LowTrace(() => $"{Id} Socket session closed via exception");
+                Close(true);
+                return false;
+            }
+            catch (ObjectDisposedException oe)
+            {
+                Logger.Current.Info($"{Id} Socket session closed, an error has occurred: {oe}");
+                Close(true);
+                return false;
+            }
+            catch (IOException ioe)
+            {
+                Logger.Current.LowTrace(() => $"{Id} Socket session closed: {ioe}");
+                Close(true);
+                return false;
+            }
+            catch (InvalidPathException)
+            {
+                Close(true);
+                return false;
+            }
+            catch (Exception e)
+            {
+                Logger.Current.Warning($"{Id} Socket session closed, an error has occurred while receiving: {e}");
+                Close(true);
+                return false;
+            }
+            finally
+            {
+                var elapsed = stopwatch?.Elapsed;
+                stopwatch?.Stop();
+//                Logger.Current.Trace($"{Id} Answer: {RemoteEndPoint} \"{Headers.Method} {Headers.Url} {Headers.Http}\" Status: {responseHeaders.Status} Size: {responseHeaders.ContentLength} Duration: {elapsed}");
+            }
+        }
+
+        #endregion
+
+        #region Fields
+
+        static int idSeed;
+
+        ServerResponseHeaders responseHeaders;
+        Stream networkStream;
+        Stopwatch stopwatch;
+        // TODO: muss kurzlebig sein!!
+        byte[] readBuffer = new byte[80000]; // Muss unter 85kB sein, sonst landet der Buffer im LOH
+
+        #endregion
+
+        #region IDisposable Support
+
+        bool disposedValue; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                    Instances.Decrement();
+                disposedValue = true;
+            }
+        }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+        }
+
+        #endregion
     }
 }
--- HttpServer/Sessions/SocketSession.cs
+++ HttpServer/Sessions/SocketSession.cs
@@ -1,16 +1,177 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Security;
 using System.Net.Sockets;
+using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using HttpServer.Exceptions;
 
 namespace HttpServer.Sessions
 {
-    public class SocketSession
+    // TODO: IDisposable und using
+    /// <summary>
+    /// Bei HTTP wird die Socket für mehrere Aufrufe wiederverwendet.
+    /// Hiermit wird eine solche Session implementiert, im Gegensatz zur logischen <see cref="RequestSession"/>, die bei jedem Aufruf neu angelegt wird
+    /// </summary>
+    class SocketSession : IDisposable
     {
+        #region Properties	
+
         public static Counter Instances { get; } = new Counter();
 
-        public static async void StartReceiving(Server server, TcpClient tcpClient, bool isSecured) { }
+        public int Id { get; private set; }
+
+        public TcpClient Client { get; private set; }
+
+        public bool UseTls { get; }
+
+        #endregion
+
+        #region Constructor	
+
+        public SocketSession(Server server, TcpClient client, bool useTls)
+        {
+            this.UseTls = useTls;
+            Id = Interlocked.Increment(ref idSeed);
+            IncrementInstance();
+            Logger.Current.LowTrace(() => $"{Id}- New {(useTls ? "secure " : "")}socket session created: - {(client.Client.RemoteEndPoint as IPEndPoint)}");
+            this.server = server;
+            this.Client = client;
+            client.ReceiveTimeout = server.Configuration.SocketTimeout;
+            client.SendTimeout = server.Configuration.SocketTimeout;
+        }
+
+        #endregion
+
+        #region Methods	
+
+        public static async void StartReceiving(Server server, TcpClient tcpClient, bool isSecured)
+        {
+            using (var session = new SocketSession(server, tcpClient, isSecured))
+                await session.ReceiveAsync();
+        }
+
+        public async Task ReceiveAsync()
+        {
+            string id = null;
+            try
+            {
+                while (true)
+                {
+                    if (networkStream == null)
+                        networkStream = UseTls ? GetTlsNetworkStream(Client) : Client.GetStream();
+
+                    using (var session = new RequestSession(server, this, networkStream))
+                    {
+                        id = session.Id;
+                        if (!await session.StartAsync())
+                            break;
+                    }
+                }
+            }
+            catch (AuthenticationException ae)
+            {
+                Logger.Current.Warning($"{Id}- An authentication error has occurred while reading socket, session: {Client.Client.RemoteEndPoint as IPEndPoint}, error: {ae}");
+            }
+            catch (Exception e) when (e is IOException || e is ConnectionClosedException || e is SocketException)
+            {
+                Logger.Current.LowTrace(() => $"{Id}- Closing socket session, reason: {e}");
+                Close();
+            }
+            catch (Exception e) when (e is ObjectDisposedException)
+            {
+                Logger.Current.Trace($"{Id}- Object disposed");
+                Close();
+            }
+            catch (Exception e)
+            {
+                Logger.Current.Warning($"{Id}- An error has occurred while reading socket, error: {e}");
+            }
+        }
+
+        public void Close() => Client.Close();
+
+        protected virtual void IncrementInstance() => Instances.Increment();
+
+        protected virtual void DecrementInstance() => Instances.Decrement();
+
+        static Stream GetTlsNetworkStream(NetworkStream stream, X509Certificate2 certificate)
+        {
+            var sslStream = new SslStream(stream);
+            // TODO: einstellbar
+            sslStream.AuthenticateAsServer(certificate, false, System.Security.Authentication.SslProtocols.Tls | System.Security.Authentication.SslProtocols.Tls11 | System.Security.Authentication.SslProtocols.Tls12, true);
+            var welches = sslStream.SslProtocol;
+            return sslStream;
+        }
+
+        Stream GetTlsNetworkStream(TcpClient tcpClient)
+        {
+            var stream = tcpClient.GetStream();
+            if (!server.Configuration.IsTlsEnabled)
+                return null;
+
+            var sslStream = new SslStream(stream);
+            sslStream.AuthenticateAsServer(server.Configuration.Certificate, false, server.Configuration.TlsProtocols, server.Configuration.CheckRevocation);
+
+            Logger.Current.LowTrace(() =>
+            {
+                Func<SslStream, string> getKeyExchangeAlgorithm = n => (int)n.KeyExchangeAlgorithm == 44550 ? "ECDHE" : $"{n.KeyExchangeAlgorithm}";
+                Func<SslStream, string> getHashAlgorithm = n =>
+                {
+                    switch ((int)n.HashAlgorithm)
+                    {
+                        case 32781:
+                            return "SHA384";
+                        case 32780:
+                            return "SHA256";
+                        default:
+                            return $"{n.HashAlgorithm}";
+                    }
+                };
+                return $"{Id}- Secure protocol: {sslStream.SslProtocol}, Cipher: {sslStream.CipherAlgorithm} strength {sslStream.CipherStrength}, Key exchange: {getKeyExchangeAlgorithm(sslStream)} strength {sslStream.KeyExchangeStrength}, Hash: {getHashAlgorithm(sslStream)} strength {sslStream.HashStrength}";
+            });
+
+            return sslStream;
+        }
+
+        #endregion
+
+        #region Fields	
+
+        static int idSeed;
+        protected Server server;
+        protected Stream networkStream;
+
+        #endregion
+
+        #region IDisposable Support
+
+        bool disposedValue; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                    DecrementInstance();
+
+                disposedValue = true;
+            }
+        }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+        }
+
+        #endregion
     }
 }
