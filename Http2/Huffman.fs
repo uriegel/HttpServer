09e9cc7d7b32bcfc1af85f6b63ec933ced3ac10e
Uwe Riegel <uriegel@hotmail.de>
2018-08-06 20:19:03 +0200

    F# assembly added

---
+++ Http2/Huffman.fs
@@ -0,0 +1,19 @@
+﻿namespace Http2
+
+open System
+
+module Huffman = 
+
+    /// Huffman coding uses a binary tree whose leaves are the input symbols
+
+    /// and whose internal nodes are the combined expected frequency of all the
+
+    /// symbols beneath them.
+
+    let symbols = 
+        [| 
+            0x23 
+            0x47
+            0x33
+        |]
+
