e839ba2c5940cc142f8f3a57e79165bebb49c706
uriegel <uriegel@hotmail.de>
2018-09-20 19:13:47 +0200

    Files continued

--- WebServer/Files.fs
+++ WebServer/Files.fs
@@ -1,5 +1,8 @@
 namespace WebServer
+open System
+open Microsoft.Extensions.Logging
 open ActivePatterns
+open System.IO
 
 module Files =
     let serve request = 
@@ -11,5 +14,33 @@ module Files =
                 query <- Some part
                 path
             | _ -> "path"
-        printfn "%s" path
+
+        let  localUrl = Uri.UnescapeDataString path            
+
+            //         var alias = Server.Configuration.Aliases.FirstOrDefault(n => url.StartsWith(n.Value));
+            // if (alias != null)
+            // {
+            //     relativePath = localURL.Substring(alias.Value.Length).Replace('/', '\\');
+
+            //     if (alias.IsRooted)
+            //         rootDirectory = alias.Path;
+            //     else
+            //         rootDirectory = Path.Combine(rootDirectory, alias.Path);
+            // }
+        let relativePath = localUrl.Replace ('/', '\\')
+        let relativePath = 
+            if relativePath.StartsWith @"\" then
+                relativePath.Substring 1
+            else
+                relativePath
+
+        // let localFile =
+        //     try
+        //         Path.Combine (rootDirectory, relativePath)
+        //     with 
+        //     | e -> 
+        //         request.categoryLogger.log LogLevel.Trace (sprintf "Invalid path: %s, %A , {e}" path e)
+        //         failwith InvalidPathException 
+            
+
         ()
